32b6d71728abe603ce525f11490f2e70ae659a19
Victor Williams <61339705+classalto@users.noreply.github.com>
2021-03-01 12:26:12 -0800

    edit create-tables.js

---
+++ data/todo-items.js
@@ -0,0 +1,23 @@
+module.exports = [
+  {
+    todo: 'clean your room',
+    completed: false,
+    user_id: 1
+  },
+  {
+    todo: 'take out the trash',
+    completed: false,
+    user_id: 1
+  },
+  {
+    id: 1,
+    todo: 'do lab assignment',
+    completed: false,
+    user_id: 2
+  },
+  {
+    todo: 'clean your room',
+    completed: false,
+    user_id: 2
+  },
+];
